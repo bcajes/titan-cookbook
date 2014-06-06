1c737058fd0d64a0b7714b3ef1294f66eee25b58
briancajes <brian.cajes@gmail.com>
2014-06-06 16:08:32 -0400

    tweaking properties template

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "brian.cajes@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures Titan Server with Cassandra storage backend and ElasticSearch index backend"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.8"
+version          "1.1.9"
 
 depends 'java'
